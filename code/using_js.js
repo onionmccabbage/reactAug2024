b0d08fc1b38b92a50c9345e6c84d5a87e68c60bf
onionmccabbage@live.com <onionmccabbage@live.com>
2024-08-12 19:24:29 +0000

    sample solution for ES6 event handling mini hcallenge

--- code/using_js.js
+++ code/using_js.js
@@ -5,13 +5,17 @@ const usr = document.getElementById('userName')
 // NB getElementById returns the ENTIRE element (including it's children)
 const go  = document.getElementById('btnGo')
 const my_robot = document.getElementById('imgRobot')
+const set_n = document.getElementById('whichSet')
+const bg_n = document.getElementById('whichBg')
 
 // a function to add a source to the image
 const doStuff = ()=>{
-    // grab whatever the user entered into the text field
+    // grab whatever the user entered into the fields
     const n = usr.value // remember every form field can have a value
+    const m = set_n.value
+    const p = bg_n.value
     // add that to the robot url: use back-tick syntax
-    const imgUrl = `https://robohash.org/${n}`
+    const imgUrl = `https://robohash.org/${n}?set=set${m}&bgset=bg${p}`
     // set the attributes on our image tag
     my_robot.setAttribute('src', imgUrl)
     my_robot.setAttribute('alt', 'yay robots!')
@@ -27,3 +31,5 @@ const handleAgree = ()=>{
 // best practice dictates we should add event listeners here (rather than in HTML)
 btn.addEventListener('click', handleAgree) // note NOT handleAgree()
 go.addEventListener('click', doStuff)
+set_n.addEventListener('change', doStuff)
+bg_n.addEventListener('change', doStuff)
