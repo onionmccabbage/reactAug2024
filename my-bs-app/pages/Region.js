2b3585bf23af20c0a39ea6d135f4d72c885df9c7
onionmccabbage@live.com <onionmccabbage@live.com>
2024-08-14 20:46:18 +0000

    sample solution for React Challenge 002

--- my-bs-app/pages/Region.js
+++ my-bs-app/pages/Region.js
@@ -1,7 +1,24 @@
-const Region =()=>{
+import { useState } from "react"
+
+const Region = () => {
+    const [region, setRegion] = useState('South America')
+    const [countryData, setCountryData] = useState({ country: 'Costa Rica', currency: 'Costa Rica Colon' })
+    const handleRegionChange = (e)=>{
+        setRegion(e.target.value)
+    }
+    
+      
     return (
         <>
-        <h4>The Region API</h4>
+            <h4>The Region API</h4>
+            <select value={region} onChange={handleRegionChange}>
+                <option>South America</option>
+                <option>North America</option>
+                <option>Northern Europe</option>
+                <option>Southern Europe</option>
+            </select>
+            <hr />
+            <p>Chosen Region: {region} </p>
         </>
     )
 }
