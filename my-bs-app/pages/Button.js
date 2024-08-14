f1417823514095e7d44b4f70634ac836a94e5451
onionmccabbage@live.com <onionmccabbage@live.com>
2024-08-14 18:29:48 +0000

    using props and state

--- my-bs-app/pages/Button.js
+++ my-bs-app/pages/Button.js
@@ -1,7 +1,20 @@
-const Button =()=>{
+import { useState } from "react"
+
+const Button =({handleParent, rand})=>{
+    // we handle model-data in state at the top of the component
+    const [stake, setStake] = useState(0) // the state is globally available within this component
+    const handleBtn = ()=>{
+        // take the current stake and increment by one
+        setStake(stake+1)
+    }
     return (
         <>
         <h4>Fun with Robot images</h4>
+        <p>Current position: {stake}</p>
+        {/* NB we do not invoke the function... no brackets */}
+        <button onClick={handleBtn}>Buy</button>
+        <button onClick={handleParent}>Go</button>
+        <img src={`https://robohash.org/${rand}`} alt={rand} />
         </>
     )
 }
--- my-bs-app/pages/panel.js
+++ my-bs-app/pages/panel.js
@@ -1,22 +1,21 @@
+import { useState } from "react"
 import Button from "./Button"
 import Region from "./Region"
 import Ticker from "./Ticker"
 
 const Panel = ()=>{
+    const [rand, setRand] = useState(55)
+    const cleverStuff = ()=>{
+        // increment the value of rand
+        setRand(rand+5)
+    }
     return (
         <>
         {/* these are really handy to ensure a single root element */}
             <h3>Working with Controlled Components</h3>
-            {/* challenge:
-                declare three more React components: Button.js, 
-                Region.js and Ticker.js
-                Inside each, declare the function/export
-                Each returns a simple <h4>
-                Instantiate one of each inside <Panel />
-            
-            */}
+            <b>The random value is {rand}</b>
             {/* remember to import these!!! */}
-            <Button />
+            <Button r={rand} handleParent={cleverStuff} />
             <Region />
             <Ticker />
         </>
