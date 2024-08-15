efce94e256f1e4f8e6f19e0399537672f8492e34
onionmccabbage@live.com <onionmccabbage@live.com>
2024-08-15 20:20:58 +0000

    prep for Friday

--- my-bs-app/pages/panel.js
+++ my-bs-app/pages/panel.js
@@ -9,15 +9,23 @@ const Panel = ()=>{
         // increment the value of rand
         setRand(rand+5)
     }
+    // this function expects two arguments (see button below)
+    const demoFn = (m, n)=>{
+        console.log(m, n)
+    }
     return (
-        <>
-        {/* these are really handy to ensure a single root element */}
+        
+        <> {/* these are really handy to ensure a single root element */}
             <h3>Working with Controlled Components</h3>
             <b>The random value is {rand}</b>
             {/* remember to import these!!! */}
             <Button r={rand} handleParent={cleverStuff} />
             <Region />
             <Ticker />
+            <hr/>
+                {/* we can pass arguments into an event handler function */}
+                <button onClick={()=>{demoFn(3, 5)}} >Demo: passing args to function</button>
+            <hr/>
         </>
     )
 }
